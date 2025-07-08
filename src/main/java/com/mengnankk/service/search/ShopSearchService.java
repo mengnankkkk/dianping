970f2cff6b297a3483228f3908fbad28b51bff05
mengnankkkk <mengnankkzhou@gmail.com>
2025-07-08 12:46:44 +0800

    "feat"(搜索):实现了分页查询的优化
    使用 分页查询优化scrollId搜索和searchAfter来实现了翻页的效果。

--- src/main/java/com/mengnankk/service/search/ShopSearchService.java
+++ src/main/java/com/mengnankk/service/search/ShopSearchService.java
@@ -1,24 +1,31 @@
 package com.mengnankk.service.search;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mengnankk.dto.SearchResult;
 import com.mengnankk.entity.Shop;
+import jodd.time.TimeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.search.OpenPointInTimeRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -49,6 +56,13 @@ public class ShopSearchService {
         this.mapper = mapper;
     }
 
+    /**
+     * 精确匹配
+     * @param keywords
+     * @return
+     * @throws IOException
+     */
+
     public List<Shop> searchShops(String keywords) throws IOException {
         SearchResponse searchResponse = null;
         try {
@@ -70,7 +84,16 @@ public class ShopSearchService {
         }
     }
 
-    public List<Shop> searchPersonWithBoolQuery(String searchText,int pageNum, int pageSize) throws IOException {
+    /**
+     * 模糊匹配+精确匹配
+     * @param searchText
+     * @param pageNum
+     * @param pageSize
+     * @return
+     * @throws IOException
+     */
+
+    public List<Shop> searchPersonWithBoolQuery(String searchText, int pageNum, int pageSize) throws IOException {
         try {
             SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
             SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
@@ -83,7 +106,7 @@ public class ShopSearchService {
 
             sourceBuilder.query(boolQueryBuilder);
             //分页
-            sourceBuilder.from((pageNum-1)*pageSize).size(pageSize);
+            sourceBuilder.from((pageNum - 1) * pageSize).size(pageSize);
             //高亮name
             HighlightBuilder highlightBuilder = new HighlightBuilder();
             highlightBuilder.field("name").preTags(HIGHLIGHT_PRE_TAG).postTags(HIGHLIGHT_POST_TAG);
@@ -92,7 +115,7 @@ public class ShopSearchService {
 
             searchRequest.source(sourceBuilder);
 
-            SearchResponse searchResponse = client.search(searchRequest,RequestOptions.DEFAULT);
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
             long totalHits = searchResponse.getHits().getTotalHits().value;
 
             return convertSearchHitsToShops(searchResponse);
@@ -101,16 +124,24 @@ public class ShopSearchService {
             return new ArrayList<>();
         }
     }
-    public List<Shop> searchPersonWithLuceneQuery(String queryString) throws IOException{
+
+    /**
+     *
+     * @param queryString
+     * @return
+     * @throws IOException
+     */
+
+    public List<Shop> searchPersonWithLuceneQuery(String queryString) throws IOException {
         try {
             SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
             SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-            QueryBuilder queryBuilders =QueryBuilders.queryStringQuery(queryString);
+            QueryBuilder queryBuilders = QueryBuilders.queryStringQuery(queryString);
 
             sourceBuilder.query(queryBuilders);
             searchRequest.source(sourceBuilder);
 
-            SearchResponse searchResponse = client.search(searchRequest,RequestOptions.DEFAULT);
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
             return convertSearchHitsToShops(searchResponse);
         } catch (IOException e) {
             log.error("查询错误");
@@ -118,15 +149,16 @@ public class ShopSearchService {
         }
 
     }
+
     private List<Shop> convertSearchHitsToShops(SearchResponse searchResponse) {
         List<Shop> shops = new ArrayList<>();
         for (SearchHit hit : searchResponse.getHits().getHits()) {
             Map<String, Object> sourceAsMap = hit.getSourceAsMap();
             Shop shop = mapper.convertValue(sourceAsMap, Shop.class);
             //高亮显示
-            if (hit.getHighlightFields()!=null){
+            if (hit.getHighlightFields() != null) {
                 HighlightField nameField = hit.getHighlightFields().get("name");
-                if (nameField!=null){
+                if (nameField != null) {
                     Text[] fragments = nameField.fragments();
                     String highlightedName = fragments[0].toString();
                     shop.setName(highlightedName);
@@ -137,28 +169,117 @@ public class ShopSearchService {
         }
         return shops;
     }
+
+    /**
+     * 监控
+     */
     @PostConstruct
-    public void monitorClusterHealth(){
+    public void monitorClusterHealth() {
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        executor.scheduleAtFixedRate(()->{
+        executor.scheduleAtFixedRate(() -> {
             try {
                 ClusterHealthRequest request = new ClusterHealthRequest();
-                ClusterHealthResponse response = client.cluster().health(request,RequestOptions.DEFAULT);
-                ClusterHealthStatus status  =response.getStatus();
+                ClusterHealthResponse response = client.cluster().health(request, RequestOptions.DEFAULT);
+                ClusterHealthStatus status = response.getStatus();
 
-                if (status==ClusterHealthStatus.RED||status==ClusterHealthStatus.YELLOW){
+                if (status == ClusterHealthStatus.RED || status == ClusterHealthStatus.YELLOW) {
                     log.error("Elasticsearch 集群状态异常: {}", status);
                     sendAlert("Elasticsearch 集群状态异常: " + status);
-                }else {
+                } else {
                     log.info("Elasticsearch 集群状态正常: {}", status);
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 log.error("检查集群健康状态失败", e);
             }
-        },0,MONITOR_INTERVAL, TimeUnit.SECONDS);
+        }, 0, MONITOR_INTERVAL, TimeUnit.SECONDS);
     }
+
     private void sendAlert(String message) {
         // TODO: 实现短信/邮件通知
         System.out.println("【告警】" + message);
+    }
+
+    /**
+     * 分页查询优化
+     * @param keywords
+     * @param pageSize
+     * @param searchAfterSortValues
+     * @param sortField
+     * @param sortOrder
+     * @return
+     * @throws IOException
+     */
+    public SearchResult<Shop> searchShopsAfter(String keywords, int pageSize, Object[] searchAfterSortValues, String sortField, String sortOrder) throws IOException {
+
+        try {
+            SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+            BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+
+            MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("name", keywords);
+            boolQueryBuilder.must(matchQueryBuilder);
+
+            sourceBuilder.query(boolQueryBuilder);
+            sourceBuilder.size(pageSize);
+
+            SortOrder order = sortOrder.equalsIgnoreCase("asc") ? SortOrder.ASC : SortOrder.DESC;
+            sourceBuilder.sort(new FieldSortBuilder(sortField).order(order));
+
+            if (searchAfterSortValues != null) {
+                sourceBuilder.searchAfter(searchAfterSortValues);
+            }
+
+            searchRequest.source(sourceBuilder);
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            List<Shop> shops = convertSearchHitsToShops(searchResponse);
+
+            Object[] nextSearchAfter = null;
+            if (shops.size() > 0) {
+                SearchHit lasthit = searchResponse.getHits().getHits()[shops.size() - 1];
+                nextSearchAfter = lasthit.getSortValues();
+            }
+            SearchResult<Shop> result = new SearchResult<>();
+            result.setItems(shops);
+            result.setNextSearchAfter(nextSearchAfter);
+            return result;
+        } catch (IOException e) {
+            log.info("使用 search_after 搜索店铺时发生IO异常", e);
+            return null;
+        }
+
+    }
+
+    /**
+     * scrollId搜索
+     * @param keywords
+     * @param pageSize
+     * @param scrollId
+     * @param scrollTime
+     * @return
+     * @throws IOException
+     */
+    public List<Shop> searchShopsByScroll(String keywords, int pageSize, String scrollId, String scrollTime) throws IOException{
+        if (scrollId==null){
+            SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+            MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("name", keywords);
+            boolQueryBuilder.must(matchQueryBuilder);
+
+            sourceBuilder.query(boolQueryBuilder);
+            sourceBuilder.size(pageSize);
+            searchRequest.source(sourceBuilder);
+            searchRequest.scroll(TimeValue.parseTimeValue(scrollTime,null,"scroll"));//设置scroll时间
+
+            SearchResponse searchResponse = client.search(searchRequest,RequestOptions.DEFAULT);
+            return convertSearchHitsToShops(searchResponse);
+        }else {
+            //后续请求
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(TimeValue.parseTimeValue(scrollTime, null, "scroll"));
+            SearchResponse searchResponse = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+            return convertSearchHitsToShops(searchResponse);
+        }
+
     }
 }
